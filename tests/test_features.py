cd7e4b4e14cb656f59aa152cd264f50e032e8493
Vihasadi <162060850+Vihasadi@users.noreply.github.com>
2024-09-30 22:03:02 -0400

    Update test_features.py

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,9 +1,8 @@
-from aim5005.features import MinMaxScaler, StandardScaler
+# Importing MinMaxScaler, StandardScaler, LabelEncoder from aim5005
+from aim5005.features import MinMaxScaler, StandardScaler, LabelEncoder  
 import numpy as np
 import unittest
 from unittest.case import TestCase
-
-### TO NOT MODIFY EXISTING TESTS
 
 class TestFeatures(TestCase):
     def test_initialize_min_max_scaler(self):
@@ -51,6 +50,8 @@ class TestFeatures(TestCase):
         data = [[0, 0], [0, 0], [1, 1], [1, 1]]
         expected = np.array([[-1., -1.], [-1., -1.], [1., 1.], [1., 1.]])
         scaler.fit(data)
+        # result not defined previously
+        result = scaler.transform(data)
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
         
     def test_standard_scaler_single_value(self):
@@ -61,7 +62,55 @@ class TestFeatures(TestCase):
         result = scaler.transform([[2., 2.]]) 
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
 
-    # TODO: Add a test of your own below this line
+# Reference links -https://stackoverflow.com/questions/21057621/sklearn-labelencoder-with-never-seen-before-values
+import unittest
+import pytest
+
+class TestLabelEncoder:
     
-if __name__ == '__main__':
-    unittest.main()
+    def test_label_encoder_init(self):
+        encoder = LabelEncoder()
+        assert isinstance(encoder, LabelEncoder), "encoder is not a LabelEncoder object"
+    
+    def test_label_encoder_fit(self):
+        encoder = LabelEncoder()
+        labels = ['soccer', 'basketball', 'tennis', 'soccer']
+        encoder.fit(labels)
+        expected_mapping = {'basketball': 0, 'soccer': 1, 'tennis': 2}  # Sorted order
+        assert encoder.label_index == expected_mapping, (
+            f"Expected label index {expected_mapping}, got {encoder.label_index}"
+        )
+    
+    def test_label_encoder_transform(self):
+        encoder = LabelEncoder()
+        labels = ['soccer', 'basketball', 'tennis']
+        encoder.fit(labels)
+        data = ['basketball', 'soccer', 'tennis']
+        expected = [0, 1, 2]  # Based on the sorted mapping
+        result = encoder.transform(data)
+        assert (np.array(result) == np.array(expected)).all(), (
+            f"Expected transformed values {expected}, got {result}"
+        )
+    
+    def test_label_encoder_fit_transform(self):
+        encoder = LabelEncoder()
+        labels = ['soccer', 'basketball', 'tennis']
+        expected = [1, 0, 2]  # Based on the sorted order
+        result = encoder.fit_transform(labels)
+        assert (np.array(result) == np.array(expected)).all(), (
+            f"Expected transformed values {expected}, got {result}"
+        )
+    
+    def test_label_encoder_single_value(self):
+        encoder = LabelEncoder()
+        labels = ['soccer', 'basketball', 'tennis']
+        encoder.fit(labels)
+
+        # Transforming a single value
+        single_value = ['soccer']
+        expected = [1]  # Based on the sorted mapping
+        result = encoder.transform(single_value)
+
+        assert (np.array(result) == np.array(expected)).all(), (
+            f"Expected transformed value {expected}, got {result}"
+        )

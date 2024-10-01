ea289f4c834b73092e19fbdcfad84e293451e990
Vihasadi <162060850+Vihasadi@users.noreply.github.com>
2024-09-30 22:01:30 -0400

    Update features.py

--- aim5005/features.py
+++ aim5005/features.py
@@ -29,9 +29,7 @@ class MinMaxScaler:
         """
         x = self._check_is_array(x)
         diff_max_min = self.maximum - self.minimum
-        
-        # TODO: There is a bug here... Look carefully! 
-        return x-self.minimum/(self.maximum-self.minimum)
+        return (x-self.minimum)/diff_max_min
     
     def fit_transform(self, x:list) -> np.ndarray:
         x = self._check_is_array(x)
@@ -40,6 +38,57 @@ class MinMaxScaler:
     
     
 class StandardScaler:
+    import numpy as np
     def __init__(self):
         self.mean = None
-        raise NotImplementedError
+        self.stdev = None
+        
+    def _check_is_array(self, x: np.ndarray) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it's not a np.ndarray and return. 
+        If it can't be cast raise an error.
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a np.ndarray"
+        return x
+        
+    def fit(self, x: np.ndarray) -> None:   
+        x = self._check_is_array(x)
+        self.mean = x.mean(axis=0)
+        self.stdev = x.std(axis=0)
+        
+    def transform(self, x: np.ndarray) -> np.ndarray:
+        """
+        Standardize the given vector.
+        """
+        x = self._check_is_array(x)
+        # Use the input x instead of self.x
+        diff_mean = x - self.mean
+        return diff_mean / self.stdev
+    
+    def fit_transform(self, x: np.ndarray) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
+
+'''Label encoder used external resource- https://www.geeksforgeeks.org/ml-label-encoding-of-datasets-in-python/,
+    https://stackoverflow.com/questions/21057621/sklearn-labelencoder-with-never-seen-before-values'''
+class LabelEncoder:
+    def __init__(self):
+        self.label_index = {}
+        self.index_label = {}
+## converting to sets to remove duplicates and also model learns about the labels
+    def fit(self, labels):
+        unique_labels = sorted(set(labels))  # Sort labels for consistent mapping
+        for index, label in enumerate(unique_labels):
+            self.label_index[label] = index
+            self.index_label[index] = label
+##  Converting original labels to indices
+    def transform(self, labels):
+        return [self.label_index[label] for label in labels]
+
+    def fit_transform(self, labels):
+        self.fit(labels)
+        return self.transform(labels)
